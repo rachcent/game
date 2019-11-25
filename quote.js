0e9739e4a736f1f22774e17642a68c9f39e2da3b
rachcent <35813484+rachcent@users.noreply.github.com>
2019-11-25 15:22:37 -0800

    q

---
+++ quote.js
@@ -0,0 +1,13 @@
+function Quote( x, y) {
+  this.x = x;
+  this.y = y;
+}
+  this.show = function() {
+    textSize(32);
+    fill(255, 0, 200);
+    text('word', this.x, this.y);
+    textAlign(CENTER);
+  }
+
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+var ship;
+var quotes = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  ship = new Ship();
+  for ( var i = 0; i < 6; i++){
+    quotes [i] = new Quote(i * 80 + 80, 60);
+  }
 }
 
 function draw() {
-  background(220);
+  background(51);
+  ship.show();
+  for ( var i = 0; i < quotes.length; i++){
+    quotes[i].show();
+}
+}
+
+function keyPressed(){
+  if (keyCode === RIGHT_ARROW) {
+    ship.move(1);
+  } else if (keyCode === LEFT_ARROW) {
+    ship.move(-1);
+  }
 }
